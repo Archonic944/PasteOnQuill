5c4b3719ffe901d1c2c1408aa45ef5222cbb45ae
Archonic <55026152+Archonic944@users.noreply.github.com>
2024-01-02 14:27:21 -0500

    Added enter functionality

--- PasteOnQuill.js
+++ PasteOnQuill.js
@@ -27,5 +27,22 @@ window.addEventListener("paste", function(event) {
     }
 }, true);
 
-    
-})();
+//when enter is pressed, click button with class quill-button
+window.addEventListener("keydown", function(event) {
+    //check if event type is keydown and key is enter
+    if (event.type === 'keydown' && event.key === 'Enter') {
+        //prevent default behavior
+        console.log("Enter event detected!")
+        //retrieve all buttons with class quill-button
+        var buttons = document.getElementsByClassName("quill-button");
+        //click the first visible one
+        for(var i = 0; i < buttons.length; i++) {
+            if(!buttons[i].hidden) {
+                buttons[i].click();
+                break;
+            }
+        }
+    }
+}, true);
+})
+
